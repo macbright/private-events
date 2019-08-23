d941d44b26bccd5caf5225b11ee6632e143f3459
Kelvin Liang <kelvin9877@gmail.com>
2019-08-23 19:49:40 +0800

    finish wrting test for User model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,14 @@ class Event < ApplicationRecord
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances
   default_scope -> { order(event_date: :asc) }
+
   scope :previous_e, -> { where('event_date < :current_date', current_date: DateTime.now) }
   scope :upcoming_e, -> { where('event_date >= :current_date', current_date: DateTime.now) }
+
+  validates :title, :description, :location, :event_date, presence: true
+  validates :title, length: { maximum: 100 }
+  
+
+
 
 end
---
+++ spec/models/users_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe User, :type => :model do
+
+  before do
+    @valid_user = User.new( name: "valid user",
+                            email: "valid_user@abc.com" )
+
+    @invalid_user = User.new( name: "invalid user",
+                            email: "invalid_user@abc.com" )
+  end                    
+
+
+  it "is valid with name, email, password " do
+    expect(@valid_user).to be_valid
+  end
+
+  it "is invalid with blank name or blank email" do
+    @invalid_user.name = ""
+    @invalid_user.email = ""
+
+    @invalid_user.valid?
+
+    expect(@invalid_user.errors[:name]).to include("can't be blank")
+    expect(@invalid_user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with incorrect email format" do
+    @invalid_user.email = "invalid@abc"
+    @invalid_user.valid?
+    expect(@invalid_user.errors[:email]).to include("is invalid")
+  end
+
+  it "return events if create event" do
+    @valid_user.events.build( title: "Example Event",
+                              description: "example",
+                              location: "online",
+                              event_date: Date.today,
+                            )
+    expect(@valid_user.events.last.title).to include("Example Event")
+  end 
+
+end
